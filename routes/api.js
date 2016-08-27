b50922d3565beb94387abceb44faa87f70e4ceb7
sophiallen <smlelevich@gmail.com>
2016-08-27 14:30:39 -0700

    api/create_dist now working

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ var dataController = require('./../dataControllers/dataControllers.js');
 
 module.exports = function(app){
 	app.post('/api/create_dist', function(req, res){
+		console.log('in api listener, heard: ' + req.body.org_name);
 		dataController.createDist(req, res);
 	});
 }
