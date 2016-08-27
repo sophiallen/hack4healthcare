90a36b2bf6faea64be098d6827ff3cc213efe398
sophiallen <smlelevich@gmail.com>
2016-08-27 11:40:34 -0700

    added keys for secrets, started index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var mongoose = require('mongoose');
+
+var app = new express();
+
+
