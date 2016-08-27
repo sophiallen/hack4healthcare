46314978c05a32c22ea526024bdb15297d75f30a
Ryan <teelrc@gmail.com>
2016-08-27 15:34:25 -0700

    update distributor schema

--- dataControllers/distributorSchema.js
+++ dataControllers/distributorSchema.js
@@ -4,10 +4,14 @@ var Schema = mongoose.Schema;
 var distributorSchema = new Schema({
 	orgName: String,
 	userType: String,
-	contactName: String,
+	contactFirstName: String,
+	contactLastName: String,
 	phone: String,
 	email: String, 
 	address: String,
+	city: String,
+	state: String,
+	zip: String,
 	hours: String,
 	created: String,
 	donationsRecieved: Array
