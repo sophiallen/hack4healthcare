d1b91cb2d5a3289c2bc124ab23db3e1e7fa1885b
sophiallen <smlelevich@gmail.com>
2016-08-27 11:48:30 -0700

    configured app

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ var mongoose = require('mongoose');
 
 var app = new express();
 
+app.set('port', process.env.PORT || 3000);
+app.use(express.static('app'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use('/api', require('cors')());
 
+
+var api = require('./routes/api.js');
+var routes = require('./routes/routes.js');
+
+app.listen(app.get('port'), function(){
+	console.log('express server started');
+});
