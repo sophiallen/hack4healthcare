c76a376535050569e59bfd895b2e6a3708ff2e5e
sophiallen <smlelevich@gmail.com>
2016-08-27 13:28:10 -0700

    working on data controller

--- dataContollers/dataControllers.js
+++ dataContollers/dataControllers.js
@@ -1,42 +1,47 @@
-// var Distributor = require('./distributorSchema.js');
-// var DonationRequest = require('./requestSchema.js');
+var Distributor = require('./distributorSchema.js');
+var DonationRequest = require('./requestSchema.js');
 
 
-// exports.createDist = function(req, res){
+exports.createDist = function(req, res){
 
-// 	var entry = new Distributor({
-// 		//id and users are auto-generated...
-// 		orgName: req.body.org_name.toLowerCase(),
-// 		contact_name
-// 	});
+	var entry = new Distributor({
+		//id and users are auto-generated...
+		orgName: req.body.org_name.toLowerCase(),
+		contactName: req.body.contact_name,
+		phone: req.body.phone,
+		email: req.body.email,
+		address: req.body.address,
+		hours: req.body.hours,
+		created: 
+	});
 
-	
+	/*
 
-// 	org_name: String,
-// 	contactName: String,
-// 	phone: String,
-// 	email: String, 
-// 	address: String,
-// 	hours: String,
-// 	created: Date,
-// 	donationsRecieved: Array
-	
+	org_name: String,
+	contactName: String,
+	phone: String,
+	email: String, 
+	address: String,
+	hours: String,
+	created: Date,
+	donationsRecieved: Array
+	*/
 
-// 	entry.save(function(err, entry){
-// 		//Save feedback to session
-// 		if (err){
-// 			res.json({
-// 				'type': 'info',
-// 				'msg': 'An error occurred: unable to add ' + capitalize(entry.name) + 'to our database.',
-// 				'display': true
-// 			});
-// 		} else {
-// 			res.json({
-// 				'type': 'success',
-// 				'msg': 'Successfully added ' + capitalize(entry.name) + ' to our list of languages',
-// 				'display': true
-// 			});
-// 		}
+	entry.save(function(err, entry){
+		//Save feedback to session
+		if (err){
+			res.json({
+				'type': 'info',
+				'msg': 'An error occurred: unable to add ' + capitalize(entry.name) + 'to our database.',
+				'display': true
+			});
+		} else {
+			res.json({
+				'type': 'success',
+				'msg': 'Successfully added ' + capitalize(entry.name) + ' to our list of languages',
+				'display': true
+			});
+		}
 
-// 	});
-// };
+	});
+};
