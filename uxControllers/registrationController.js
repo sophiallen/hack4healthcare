0b1aca5b90c287193ef6b8f36f4ee5c5e42a59a0
Bailey June <baileyjune06@gmail.com>
2016-08-27 15:03:20 -0700

    reg controller

--- uxControllers/registrationController.js
+++ uxControllers/registrationController.js
@@ -3,13 +3,13 @@ var registration = angular.module('registration',[]);
 registration.controller('RegistrationController', ['$scope', function($scope){
 	$scope.register = function()
 	{
-		if ($user_type = "distributer")
+		if ($user_type = "distributor")
 		{
-
+			$window.location = "custom-homepage-distributor";
 		}
 		else
 		{
-
+			$window.location = "custom-homepage-collector";
 		}
 	};
 }])
