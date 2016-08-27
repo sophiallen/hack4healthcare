385691c06801dd33a0132fe8a3654bb56c7f829c
sophiallen <smlelevich@gmail.com>
2016-08-27 12:27:16 -0700

    temporarily moved routing to index.js file

--- index.js
+++ index.js
@@ -20,7 +20,13 @@ app.get('/', function(req, res){
 		res.type('text/html');
 		var options = {root: __dirname + '/app/views'};
 		res.sendFile('index.html', options);
-	});
+});
+
+//handle 404 errors
+app.use(function(req, res){
+	res.type('text/plain');
+	res.status(404).send('404: file not found');
+});
 
 app.listen(app.get('port'), function(){
 	console.log('express server started');
