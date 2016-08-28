86682c80ac26a9bee2531f917b853adba13d1336
Ryan <teelrc@gmail.com>
2016-08-27 18:00:43 -0700

    users

--- app/uxControllers/loginController.js
+++ app/uxControllers/loginController.js
@@ -1,6 +1,13 @@
 var login = angular.module('loginApp',[]);
 
 login.controller('loginController', function($scope, $http, $window){
-	
+	$scope.logon = function(){
+		if($scope.user == "dist1" && $scope.password == "dist1"){
+			$window.location.href = "custom-homepage-distributor.html";
+		}
+		else if($scope.user == "supp1" && $scope.password == "supp1"){
+			$window.location.href = "supplier.html";
+		}
+	};
 	
 })
