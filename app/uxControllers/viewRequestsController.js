c6951987c3611779fdfd148e0af0dc296f3ab18e
kfontes <kfontes.uw@gmail.com>
2016-08-27 18:32:52 -0700

    added table data

--- app/uxControllers/viewRequestsController.js
+++ app/uxControllers/viewRequestsController.js
@@ -5,8 +5,9 @@ requests.controller('viewRequestsController', function($scope, $http, $window){
   
   $scope.requestList =
   [
-    {org: 'sample', tampons: '5', pads: '3', cups: '2', urgency: 'Immediately'},
-    {org: 'sample', tampons: '5', pads: '3', cups: '2', urgency: 'Immediately'},
-    {org: 'sample', tampons: '5', pads: '3', cups: '2', urgency: 'Immediately'}    
+    {org: 'WYMCA', tampons: '5', pads: '20', cups: '12', urgency: 'Immediately'},
+    {org: 'Safe Place for Teens', tampons: '15', pads: '30', cups: '2', urgency: 'Anytime'},
+    {org: 'Women\'s Shelter', tampons: '10', pads: '10', cups: '5', urgency: 'Immediately'},  
+    {org: 'Starbucks - 9th and Pike', tampons: '30', pads: '40', cups: '20', urgency: '1 - 2 weeks'}      
   ]
 });
