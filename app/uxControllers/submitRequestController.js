2f6b53f0b4d9bc07f6e18f89b3f7c4b536c37fcc
Adrienne Grieco <adrienne.grieco@gmail.com>
2016-08-27 18:27:00 -0700

    submit button fix

--- app/uxControllers/submitRequestController.js
+++ app/uxControllers/submitRequestController.js
@@ -2,5 +2,7 @@ var submit = angular.module('submitApp',[]);
 
 submit.controller('submitController', function($scope, $http, $window){
 	console.log("I am in the submit controller.");
-	
+	$scope.submit=function(){
+		$window.location.href = "custom-homepage-distributor.html";
+	};
 })
